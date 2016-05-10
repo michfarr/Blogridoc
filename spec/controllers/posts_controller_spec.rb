0ba6bf8eae63e9506d8f59418bbc9ce124335336
michfarr <etnunc@protonmail.com>
2016-05-10 16:30:18 +0200

    Add remaining actions to Posts Controller and implement test for show
    Add show view as well

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -4,9 +4,18 @@ RSpec.describe PostsController, type: :controller do
   describe "GET index" do
     let(:posts) { create_list(:post, 3) }
 
-    it "assigns all Posts to @Posts" do
+    it "assigns all posts to @posts" do
       get :index
       expect(assigns(:posts)).to eq posts
+    end
+  end
+
+  describe "GET show" do
+    let(:post) { create(:post) }
+
+    it "assigns the the post to @post" do
+      get :show, id: post.to_param
+      expect(assigns(:post)).to eq post
     end
   end
 end
