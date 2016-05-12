8316c9a9298d0ec8c2a3855deee66132801123fd
michfarr <etnunc@protonmail.com>
2016-05-12 10:33:15 +0200

    Posts controller tests (#12)
    * Add remaining actions to Posts Controller and implement test for show
    
    Add show view as well
    
    * Add controller test for New and edit with the corresponding views

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,12 +1,52 @@
 require 'rails_helper'
 
 RSpec.describe PostsController, type: :controller do
-  describe "GET index" do
+  describe "GET #index" do
     let(:posts) { create_list(:post, 3) }
 
-    it "assigns all Posts to @Posts" do
+    it "assigns all posts to the instance var @posts" do
       get :index
       expect(assigns(:posts)).to eq posts
+    end
+
+    it "renders the index template" do
+      expect(response).to render_template{"index"}
+    end
+  end
+
+  describe "GET #show" do
+    let(:post) { create(:post) }
+
+    it "assigns the post to the instance var @post" do
+      get :show, id: post.to_param
+      expect(assigns(:post)).to eq post
+    end
+
+    it "renders the show template" do
+      expect(response).to render_template{"show"}
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new post as the instance var @post" do
+      get :new
+      expect(assigns(:post)).to be_a_new(Post)
+    end
+
+    it "renders the new template" do
+      expect(response).to render_template{"new"}
+    end
+  end
+
+  describe "GET #edit" do
+    let(:post) { create(:post) }
+
+    it "assigns the selected post to the instance var @post" do
+      get :edit, id: post.to_param
+    end
+
+    it "renders the edit template" do
+      expect(response).to render_template{"edit"}
     end
   end
 end
